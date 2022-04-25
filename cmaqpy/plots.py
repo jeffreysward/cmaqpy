370b3ab02bc2e4c9ad50612178c3211410230361
Jeff Sward <jas983@cornell.edu>
2022-04-25 17:17:23 -0400

    new plotting function for lambert conformal grids as well as funcitons that get proj info and domain boundary info

--- cmaqpy/plots.py
+++ cmaqpy/plots.py
@@ -1,16 +1,148 @@
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
 from matplotlib import cm
+from matplotlib import colors
 import monet as m
+import numpy as np
 import pandas as pd
+import wrf as wrfpy
+import xarray as xr
+
+
+def get_proj(ds):
+    """
+    Extracts information about the CMAQ grid projection from the proj4_srs attribute
+    in an output file dataset.
+
+    :param ds:
+    :return:
+    """
+    proj_params = ds.proj4_srs
+    proj_params = proj_params.replace(' ', '')
+    proj_params = proj_params.split('+')
+    proj = proj_params[1].split('=')[1]
+    truelat1 = float(proj_params[2].split('=')[1])
+    truelat2 = float(proj_params[3].split('=')[1])
+    central_latitude = float(proj_params[4].split('=')[1])
+    central_longitude = float(proj_params[5].split('=')[1])
+
+    if proj == 'lcc':
+        cartopy_crs = ccrs.LambertConformal(central_longitude=central_longitude,
+                                            central_latitude=central_latitude,
+                                            standard_parallels=[truelat1, truelat2])
+        return cartopy_crs
+    else:
+        raise ValueError('Your projection is not the expected Lambert Conformal.')
+
+
+def get_domain_boundary(ds, cartopy_crs):
+    """
+    Finds the boundary of the WRF domain.
+
+    :param ds:
+    :param cartopy_crs:
+    :return:
+    """
+    # Rename the lat-lon corrdinates to get wrf-python to recognize them
+    variables = {'latitude': 'XLAT',
+                 'longitude': 'XLONG'}
+    try:
+        ds = xr.Dataset.rename(ds, variables)
+    except ValueError:
+        print(f'Variables {variables} cannot be renamed, '
+                f'those on the left are not in this dataset.')
+
+    # I need to manually convert the boundaries of the WRF domain into Plate Carree to set the limits.
+    # Get the raw map bounds using a wrf-python utility
+    raw_bounds = wrfpy.util.geo_bounds(ds)
+
+    # Get the projected bounds telling cartopy that the input coordinates are lat/lon (Plate Carree)
+    projected_bounds = cartopy_crs.transform_points(ccrs.PlateCarree(),
+                                                    np.array([raw_bounds.bottom_left.lon, raw_bounds.top_right.lon]),
+                                                    np.array([raw_bounds.bottom_left.lat, raw_bounds.top_right.lat]))
+    return projected_bounds
+
+
+def conc_map(plot_var, cmap=cm.get_cmap('bwr'), figsize=(8,8), ax=None, cartopy_crs=None, proj_bounds=None,
+    vmin=-1, vmax=1, cbar_args={}, savefig=False, figpath='conc_map.png'):
+    """
+    Creates a filled colormap across the full domain in the native (Lambert
+    Conformal) map projection.
+    """
+    if ax is None:
+        # Create a figure
+        fig = plt.figure(figsize=figsize)
+
+        # Set the GeoAxes to the projection used by WRF
+        ax = fig.add_subplot(1, 1, 1, projection=cartopy_crs)
+
+    # Normalize the values, so that the colorbar plots correctly
+    norm = colors.Normalize(vmin=vmin, vmax=vmax)
+
+    # Create the pcolormesh 
+    cn = ax.pcolormesh(wrfpy.to_np(plot_var.longitude), wrfpy.to_np(plot_var.latitude), wrfpy.to_np(plot_var),
+                       transform=ccrs.PlateCarree(), 
+                       cmap=cmap,
+                       norm=norm,
+                       )
+    if proj_bounds is not None:
+        # Format the projected bounds so they can be used in the xlim and ylim attributes
+        proj_xbounds = [proj_bounds[0, 0], proj_bounds[1, 0]]
+        proj_ybounds = [proj_bounds[0, 1], proj_bounds[1, 1]]
+
+        # Finally, set the x and y limits
+        ax.set_xlim(proj_xbounds)
+        ax.set_ylim(proj_ybounds)
+
+    # Download and add the states, coastlines, and lakes
+    shapename = 'admin_1_states_provinces_lakes'
+    states_shp = shpreader.natural_earth(resolution='10m',
+                                        category='cultural', 
+                                        name=shapename)
+    # Add features to the maps
+    ax.add_geometries(
+        shpreader.Reader(states_shp).geometries(),
+        ccrs.PlateCarree(),
+        facecolor='none',
+        linewidth=.5, 
+        edgecolor="black"
+        )
+
+    # Add features to the maps
+    # ax.add_feature(cfeature.LAKES)
+    # ax.add_feature(cfeature.OCEAN)
+
+    # Add color bars
+    if "cbar_ticks" not in cbar_args:
+        cbar_args["cbar_ticks"] = None
+    if "cbar_label" not in cbar_args:
+        cbar_args["cbar_label"] = 'Concentration'
+    if "shrink" not in cbar_args:
+        cbar_args["shrink"] = 1
+    if "pad" not in cbar_args:
+        cbar_args["pad"] = 0.05
+
+    cbar = plt.colorbar(cn,
+                        ax=ax,
+                        ticks=cbar_args["cbar_ticks"],
+                        label=cbar_args["cbar_label"],
+                        shrink=cbar_args["shrink"],
+                        pad=cbar_args["pad"]
+                        )
+
+    # Save the figure(s)
+    if savefig:
+        plt.savefig(figpath, dpi=300, transparent=True, bbox_inches='tight')
 
 
 def pollution_plot(da, vmin=0, vmax=12, cmap=cm.get_cmap('YlOrBr'),
                    extent=None, cbar_label='PM$_{2.5}$ ($\mu g/m^{3}$)', figsize=(15,7),
                    titlestr='Title', savefig=False, figpath='./pollution_plot.png'):
     """
-    Creates a filled colormap across a user-defined section of the domain 
-    (defaults to the full domain).
+    Creates a filled colormap using the Plate Carree projectsion across a 
+    user-defined section of the domain (defaults to the full domain) using 
+    the monit package paradigm.
     """
     if extent is None:
         extent = [da.longitude.min(), da.longitude.max(), da.latitude.min(), da.latitude.max() - 2]
