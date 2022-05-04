430d56fb5d270616fa5de580c11f8adb0175f39d
Jeff Sward <jas983@cornell.edu>
2022-05-04 11:56:25 -0400

    Updated docs

--- cmaqpy/utils.py
+++ cmaqpy/utils.py
@@ -15,10 +15,11 @@ def format_date(in_date):
     """
     Formats an input date so that it can be correctly written to the namelist.
 
+    Parameters
+    ----------
     :param in_date : string
         string specifying the date
     :return: datetime64 array specifying the date
-
     """
     for fmt in ('%b %d %Y', '%B %d %Y', '%b %d, %Y', '%B %d, %Y',
                 '%m-%d-%Y', '%m.%d.%Y', '%m/%d/%Y',
@@ -38,6 +39,8 @@ def strfdelta(tdelta, fmt='{H:02}h {M:02}m {S:02}s', inputtype='timedelta'):
     formatted string, just like the stftime() method does for datetime.datetime
     objects.
 
+    Parameters
+    ----------
     :param tdelta: datetime.timedelta object
         defining the difference in time that you would like to format.
     :param fmt: string
@@ -90,6 +93,20 @@ def strfdelta(tdelta, fmt='{H:02}h {M:02}m {S:02}s', inputtype='timedelta'):
 def write_to_template(template_path, txt_to_write, id='%REPLACE_HERE%'):
     """
     Replace a placeholder ID within a template file with desired text.
+
+    Parameters
+    ----------
+    :param template_path: string
+        Full path to the template file. You'll want to copy the template 
+        file from the `cmaqpy/templates` directory, so you can reuse the 
+        template. 
+    :param txt_to_write: string
+        Text to be written to the template file. Note that you can include
+        comments, line breaks, etc. 
+    :param id: string
+        Identifier that will be overwritten with the text contained within 
+        the `text_to_write` parameter. Obviously, this string should be unique
+        so you don't accidentally overwrite the wrong thing. 
     """
     try:
         with open(template_path, 'r') as template:
@@ -109,10 +126,11 @@ def read_script(file):
     """
     Opens a namelist file within a context manager.
 
+    Parameters
+    ----------
     :param file: string
         Path to the namelist file you wish to open.
     :return: file object
-
     """
     with open(file, 'r') as script:
         return script.read()
@@ -122,11 +140,12 @@ def read_last(file_name, n_lines=1):
     """
     Reads the last line of a file.
 
+    Parameters
+    ----------
     :param file_name: string
         Complete path of the file that you would like read.
     :return last_line: string
         Last line of the input file.
-
     """
     try:
         with open(file_name, mode='r') as infile:
@@ -145,6 +164,9 @@ def read_last(file_name, n_lines=1):
 def remove_dir(directory, verbose=False):
     """
     This function utilized an exception clause to delete a directory.
+
+    Parameters
+    ----------
     :param directory: string
         complete path to the directory to be removed.
     :param verbose: boolean (default=False)
@@ -160,6 +182,9 @@ def remove_dir(directory, verbose=False):
 def make_dirs(directory):
     """
     Checks if a directory exists and creates it if it doesn't.
+
+    Parameters
+    ----------
     :param directory: string
         complete path to the directory to be created. 
     """
@@ -181,6 +206,20 @@ def get_rep_dates(smk_dates_dir, dates_list, date_type='  mwdss_N'):
     NOTE: this woudld be slightly faster if I didn't reopen the file each time, 
     but it's such a small fraction of the time for CCTM that I'm not
     going to worry about it now.
+
+    Parameters
+    ----------
+    :param smk_dates_dir: string
+        Full path to the directory where the smoke merge date text files are located.
+    :param date_list: list of `Timestamp` (or `datetime.datetime`)
+        Simulation dates for which you want the corresponding representative dates.
+    :param date_type: string
+        Label from the `smk_merge_dates_YYYYMM.txt` file that you want to extract. 
+        Note that SMOKE appends white space to the beginning of these, so you have 
+        to include this white space for this function to run successfully (see function
+        header).
+    :retrun: `pandas.DatetimeIndex`
+        Index of representative dates. 
     """
     rep_days = []
     # Loop through each day in the input list and append the respective representative day to the list
@@ -204,8 +243,22 @@ def convert_tz_xr(ds, input_tz='UTC', output_tz='US/Eastern', time_coord='time')
     """
     Converts an xarray dataset from one timezone to another.
 
-    Note that this will not work if you have problematic features within your simulation
-    (e.g., time changes). This is why xarray hasn't integrated this feature yet. 
+    Note that this will not work if you have problematic date features within your 
+    simulation (e.g., time changes). This is why xarray hasn't integrated this feature yet. 
+
+    Parameters
+    ----------
+    :param ds: `xarray.DataSet`
+        DataSet that you want converted from one time zone to another.
+    :param input_tx: string
+        String identifying the current time zone of the DataSet.
+    :param output_tx: string
+        String identifying the desired time zone of the DataSet.
+    :param time_coord: string
+        Name of the coordinate holding date/time information within `xarray.DataSet`
+        that you passed in with `ds`.
+    :return: `xarray.DataSet`
+        DataSet with updated time zone reflected in the time coordinate. 
     """
     tidx_in = ds.time.to_index().tz_localize(tz=input_tz)
     ds.coords[time_coord] = tidx_in.tz_convert(output_tz).tz_localize(None)
